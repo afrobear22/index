9de8cc0427da1f651c4832911a54959a512a2486
Blazhe Jakimovski <91224265+afrobear22@users.noreply.github.com>
2021-12-16 17:04:29 +0100

    Add files via upload

---
+++ JavaScript/12/js/script.js
@@ -0,0 +1,96 @@
+// OBJECTS
+
+// var movie = {
+//     title: "Bicycle Thieves",
+//     year: 1948
+// };
+
+// console.log(movie);
+
+// console.log(movie.title);
+// console.log(movie.year);
+
+// var user = {
+//     first_name: "Blazhe",
+//     last_name: "Jakimovski",
+//     e_mail: "jakimovski22@gmail.com",
+//     age: 28
+// }
+
+// var academy = {
+//     name: "Semos Academy",
+//     module: "Javascript",
+//     trainer: "Mario Petkovski"
+// }
+// console.log("user", user);
+
+// console.log("Hello World. My name is " + user.first_name + " " + user.last_name + " and I'm " + user.age + " old. I'm being thought " + academy.module + " by " + academy.trainer + " in the " + academy.name);
+
+// console.log(`Hello World. My name is ${user.first_name} ${user.last_name} and I'm ${user.age} years old. I'm being thought ${academy.module} by ${academy.trainer} in the ${academy.name}`);
+
+// var hotel = {
+//     name: "Hilton",
+//     roomNumber: 237,
+//     hasVacancy: false,
+//     managment: {
+//         manager: "Blaze",
+//         email: "blaze@blaze.com"
+//     }
+// }
+
+// console.log("hotel", hotel)
+// console.log("manager name", hotel.managment.manager);
+// =================================
+// +++++++++++++++++++++++++++++++++
+
+// ARRAYS
+
+// var movies = [ "Bicycle Thieves", "Notorious", "The friends of Eddie Coyle"];
+
+// console.log("movies",movies);
+
+// console.log(movies[1]);
+
+// var films = [
+//     {
+//         name: "Matrix",
+//         year: 1999,
+//         genres: ["science fiction", "action"],
+//         director: {
+//             dir_name: "Lana Wachowskis",
+//             dateOfBirth: "June 21, 1965"
+//         },
+//         hasOscar: true,
+//         actors: [ "Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving", "Joe Pantoliano."]
+//     },
+
+//     {
+//         name: "Fight Club",
+//         year: 1999,
+//         genres: ["Thriller", "Drama"],
+//         director: {
+//             dir_name: "David Fincher",
+//             dateOfBirth: "August 28, 1962"
+//         },
+//         hasOscar: false,
+//         actors: [ "Brad Pitt", "Edward Norton", " Helena Bonham Carter"]
+//     },
+
+//     {
+//         name: "Joker",
+//         year: 2019,
+//         genres: ["Crime", "Drama"],
+//         director: {
+//             dir_name: "Todd Phillips",
+//             dateOfBirth: "December 20, 1970"
+//         },
+//         hasOscar: true,
+//         actors: [ "Joaquin Phoenix", " Robert De Niro", "Zazie Beetz"]
+//     }
+// ]
+
+
+// console.log("films", films);
+
+// console.log(`The movie ${films.name} is a ${films.genres} movie and was released in ${films.year} directed by ${films.director.dir_name} born in ${films.director.dateOfBirth}. The lead actors in the movie are ${films.actors}. The ${films.name} has won an oscar.`)
+
